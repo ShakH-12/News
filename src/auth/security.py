b5a63d87b5346216f02545c8dc56bb3782a3f892
ShakH-12 <shakh8778@gmail.com>
2026-02-08 15:35:45 +0000

    News in FastAPI

--- src/auth/security.py
+++ src/auth/security.py
@@ -1,9 +1,13 @@
 from authx import AuthX, AuthXConfig
 from passlib.context import CryptContext
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 config = AuthXConfig()
-config.JWT_SECRET_KEY = "SECRETKEY"
-config.JWT_ACCESS_COOKIE_NAME = "my_access_token"
+config.JWT_SECRET_KEY = os.getenv("SECRET_KEY")
+config.JWT_ACCESS_COOKIE_NAME = os.getenv("JWT_ACCESS_COOKIE_NAME")
 config.JWT_TOKEN_LOCATION = ["cookies"]
 
 security = AuthX(config=config)
